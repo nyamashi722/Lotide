cef75fd758ca033dec61b8482b827ce5c7156b28
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-09-18 16:42:36 -0400

    created working function

---
+++ head.js
@@ -0,0 +1,11 @@
+function assertEqual(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+function head(array) {
+  return array[0];
+}
