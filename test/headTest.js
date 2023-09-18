7d3b7c1c7183e525e932fb19704192f95484ae19
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-09-18 16:44:20 -0400

    function with test cose

---
+++ test/headTest.js
@@ -0,0 +1,16 @@
+function assertEqual(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+function head(array) {
+  return array[0];
+}
+
+//TEST CODE
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
