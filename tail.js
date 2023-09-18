46277015458fa6665307958c374baff935d2266f
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-09-18 17:00:23 -0400

    created working function

---
+++ tail.js
@@ -0,0 +1,12 @@
+function assertEqual(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+function tail(array) {
+  let arrayTail = array.slice(1);
+  return arrayTail;
+}
