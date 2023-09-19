49cc03d74d445474ca5a9c410731771d047551c5
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-09-19 16:46:24 -0400

    working array comparison function

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+function assertEqual(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+function eqArrays(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+}
