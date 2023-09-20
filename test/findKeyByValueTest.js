934fac1601fbaab3b817510b2ff4b654c7143bf2
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-09-20 16:17:21 -0400

    findKeyByValue function with test cases

---
+++ test/findKeyByValueTest.js
@@ -0,0 +1,25 @@
+function assertEqual(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+function findKeyByValue(object, value) {
+  for (let key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+}
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), "comedy");
