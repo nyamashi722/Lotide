6f51141c697a3a46e50fe008b5d99ca050c1f4f9
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-09-20 17:23:38 -0400

    function to compare two oobjects when their values are primitive or an array

---
+++ eqObjects.js
@@ -0,0 +1,35 @@
+function assertEqual(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+function eqArrays(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function eqObjects(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } else {
+    for (let item in object1) {
+      if (Array.isArray(object1[item]) && (Array.isArray(object2[item]))) {
+        eqArrays(object1[item], object2[item]);
+      } else if (object1[item] !== object2[item]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
