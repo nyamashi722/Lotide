60bfb64a9879bc741da562364e1e04b614046e72
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-09-20 18:12:23 -0400

    function that compares objects and logs a message to the console whether they are the same or not

---
+++ assertObjectsEqual.js
@@ -0,0 +1,35 @@
+function assertObjectsEqual(actual, expected) {
+  const inspect = require("util").inspect;
+  if (Object.keys(actual).length !== Object.keys(expected).length) {
+    console.log(`🔴Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  } else {
+    for (let item in actual) {
+      if (Array.isArray(actual[item]) && (Array.isArray(expected[item]))) {
+        if (eqArrays(actual[item], expected[item]) === true) {
+          console.log(`✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+        } else if (eqArrays(actual[item], expected[item]) === false) {
+          console.log(`🔴Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+        }
+        return;
+      } else if (actual[item] !== expected[item]) {
+        console.log(`🔴Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+        return;
+      }
+    }
+    console.log(`✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  }
+}
+
+function assertArraysEqual(actual, expected) {
+  if (actual.length !== expected.length) {
+    console.log(`🔴Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        console.log(`🔴Assertion Failed: ${actual} !== ${expected}`);
+        return;
+      }
+    }
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  }
+}
