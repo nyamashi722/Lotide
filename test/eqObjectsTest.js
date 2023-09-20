8b71a7496deb23b2a7dee98153d06bc49cde2f46
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-09-20 17:24:42 -0400

    edited function

--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -21,18 +21,18 @@ function eqArrays(firstArray, secondArray) {
 
 function eqObjects(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false
+    return false;
   } else {
     for (let item in object1) {
       if (Array.isArray(object1[item]) && (Array.isArray(object2[item]))) {
-        eqArrays(object1[item], object2[item])
+        eqArrays(object1[item], object2[item]);
       } else if (object1[item] !== object2[item]) {
-        return false
+        return false;
       }
     }
-    return true
+    return true;
   }
-};
+}
 
 const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
 const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
