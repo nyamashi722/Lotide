a75c516cb7096ebe0fb5e8264699c6899a746027
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-09-21 16:36:32 -0400

    takeUntil function takes an array and returns items from the beginning of input array until callback returns truthy value

---
+++ takeUntil.js
@@ -0,0 +1,10 @@
+const takeUntil = function(array, callback) {
+  let results = [];
+  for (let item of array) {
+    if (callback(item) === false) {
+      results.push(item);
+    } else {
+      return results;
+    }
+  }
+};
