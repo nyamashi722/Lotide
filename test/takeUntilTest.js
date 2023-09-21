062cd38e565921cca0e60063a561374eb9d73b05
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-09-21 16:34:22 -0400

    takeUntil function with test cases

---
+++ test/takeUntilTest.js
@@ -0,0 +1,44 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === false) {
+    console.log(`🔴Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const takeUntil = function(array, callback) {
+  let results = [];
+  for (let item of array) {
+    if (callback(item) === false) {
+      results.push(item);
+    } else {
+      return results;
+    }
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+console.log(assertArraysEqual((results1),[ 1, 2, 5, 7, 2 ]));
