c6d4c2ecf7384bbbd080da40d23bcaca3e6cf45d
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-09-21 15:53:36 -0400

    map function with test cases

---
+++ test/mapTest.js
@@ -0,0 +1,35 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === false) {
+    console.log(`🔴Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+console.log(assertArraysEqual(results1,[ 'g', 'c', 't', 'm', 't' ] ))
