c6f673fea49a4abe5fb2d16edeb101e38b6ae680
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-09-24 12:07:34 -0400

    created flatten function that takes an array of various levels of nesting and returns a flattened version of the input array

---
+++ flatten.js
@@ -0,0 +1,46 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === false) {
+    console.log(`🔴Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  }
+}
+
+const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/*const flatten = function(array) {
+  let result = [];
+  array.forEach(item => {
+    if (Array.isArray(item)) {
+      for (let num of item) {
+        result.push(num)
+      }
+    } else {
+      result.push(item)
+    }
+  });
+  return result
+}*/
+
+const flatten = function(array) {
+  let result = [];
+  array.forEach(item => {
+    if (Array.isArray(item)) {
+      result = result.concat(flatten(item))
+    } else {
+      result.push(item)
+    }
+  });
+  return result
+}
