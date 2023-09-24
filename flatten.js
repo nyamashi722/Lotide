1d9b5a0aa9e84565242d8a6e366744d88331eb95
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-09-24 12:07:52 -0400

    edited function

--- flatten.js
+++ flatten.js
@@ -37,10 +37,10 @@ const flatten = function(array) {
   let result = [];
   array.forEach(item => {
     if (Array.isArray(item)) {
-      result = result.concat(flatten(item))
+      result = result.concat(flatten(item));
     } else {
-      result.push(item)
+      result.push(item);
     }
   });
-  return result
+  return result;
 }
